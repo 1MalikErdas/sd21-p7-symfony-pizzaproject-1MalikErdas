d91db245ae68103d2acd65f2e17ea7ea28022e18
1MalikErdas <302891760@student.rocmondriaan.nl>
2023-03-22 12:56:15 +0100

    database

--- src/Controller/contactController.php
+++ src/Controller/contactController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController extends AbstractController
+class contactController extends AbstractController
 {
-    #[Route('/lucky/number')]
+    #[Route('/pizza/contact')]
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return $this->render('lucky/number.html.twig', [
+        return $this->render('pizza/contact.html.twig', [
             'number' => $number,
         ]);
     }
--- src/Controller/pizzaController.php
+++ src/Controller/pizzaController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController extends AbstractController
+class pizzaController extends AbstractController
 {
-    #[Route('/lucky/number')]
+    #[Route('/pizza/home')]
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return $this->render('lucky/number.html.twig', [
+        return $this->render('pizza/home.html.twig', [
             'number' => $number,
         ]);
     }
